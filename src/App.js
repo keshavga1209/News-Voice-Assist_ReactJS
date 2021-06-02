7a41905c4c8246f564dd2686700e04e1057ca9ab
Keshav Gautam <keshavg1209@gmail.com>
2021-06-03 00:46:31 +0530

    Some changes left

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import wordsToNumbers from 'words-to-numbers';
+import React, {useState,useEffect} from 'react';
+import NewsCards from './components/NewsCards/NewsCards'
+import useStyles from './styles.js';
+
+const alanKey = 'dfd5dec54cb8563db5cc7393c7d5a5ec2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 function App() {
+  const [newsArticles, setnewsArticles] = useState([]);
+  const [activeArticle, setactiveArticle] = useState(-1);
+  const classes = useStyles();
+  useEffect(() => {
+    alanBtn(
+      {
+        key: alanKey,
+        onCommand:({command, articles, number})=>{
+          if(command==='newHeadlines'){
+            //console.log("hi");
+            setnewsArticles(articles); 
+            setactiveArticle(-1);
+          }
+          else if(command==='highlight'){
+            setactiveArticle((prevArticle)=>prevArticle +1 )
+          }
+          else if (command === 'open') {
+            const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+            const article = articles[parsedNumber - 1];
+  
+            if (parsedNumber > articles.length) {
+              alanBtn().playText('Please try that again...');
+            } else if (article) {
+              window.open(article.url, '_blank');
+              alanBtn().playText('Opening...');
+            } else {
+              alanBtn().playText('Please try that again...');
+            }
+          }
+        }
+      }
+    )
+  }, [])
+
+                                       
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = {classes.logoContainer}>
+    <img src = "https://cdn.dribbble.com/users/2501555/screenshots/5965492/flo-motion_5sec.gif" className = {classes.logoContainer} alt = "HII" />
+    </div>
+    <br/><br/>
+     <NewsCards articles = {newsArticles} activeArticle = {activeArticle}/> 
     </div>
   );
 }
